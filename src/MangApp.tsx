abcdad98c51cd523183946983c6902c731208fd9
pablo bernal moreno <pbm1998@hotmail.com>
2021-10-19 21:28:16 -0500

    Mockign MCard Component

--- src/MangApp.tsx
+++ src/MangApp.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React from "react";
+import { MCard } from "./components/MCard/MCard";
 
 function MangApp() {
   return (
     <div className="MangApp">
+      Hola
+      <MCard />
     </div>
   );
 }
